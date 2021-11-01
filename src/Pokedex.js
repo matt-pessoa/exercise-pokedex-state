5bcfb64f07c5b2398ab5d3a629e8eaee9e276864
matt-pessoa <matheuspessoafonseca@gmail.com>
2021-11-01 19:36:52 -0300

    Cria componente Filter

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,58 +1,63 @@
-import React from 'react';
-import Pokemon from './Pokemon';
-import data from './data';
-import './Pokedex.css';
+import React from "react";
+import Pokemon from "./Pokemon";
+import data from "./data";
+import Filter from "./Filter";
+import "./Pokedex.css";
 
 class Pokedex extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      index: 0,
-      pokemon: data.map((obj, index) => (
-        <Pokemon key={index} pokemon={obj} />
-      )),
-    }
-    this.handleNext = this.handleNext.bind(this)
-    this.handlePrevious = this.handlePrevious.bind(this)
-  }
+	constructor() {
+		super();
+		this.state = {
+			index: 0,
+			pokemon: data.map((obj, index) => <Pokemon key={index} pokemon={obj} />),
+		};
+		this.handleNext = this.handleNext.bind(this);
+		this.handlePrevious = this.handlePrevious.bind(this);
+	}
 
-  handleNext() {
-    if (this.state.index === this.state.pokemon.length - 1) {
-      this.setState({ index: 0 })
-    } else {
-      this.setState(prevState => ({
-        index: prevState.index + 1,
-      }))
-    }
-  }
+	handleNext() {
+		if (this.state.index === this.state.pokemon.length - 1) {
+			this.setState({ index: 0 });
+		} else {
+			this.setState((prevState) => ({
+				index: prevState.index + 1,
+			}));
+		}
+	}
 
-  handlePrevious() {
-    if (this.state.index === 0) {
-      this.setState({ index: this.state.pokemon.length - 1 })
-      console.log(this.state.index)
-    } else {
-      this.setState(prevState => ({
-        index: prevState.index - 1,
-      }))
-    }
-  }
-  render() {
-    return (
-      <section className='pokedex'>
-        <div className='container'>
-          <header>
-            <h1>Pokédex</h1>
-          </header>
+	handlePrevious() {
+		if (this.state.index === 0) {
+			this.setState({ index: this.state.pokemon.length - 1 });
+			console.log(this.state.index);
+		} else {
+			this.setState((prevState) => ({
+				index: prevState.index - 1,
+			}));
+		}
+	}
+	render() {
+		return (
+			<section className="pokedex">
+				<div className="container">
+					<header>
+						<h1>Pokédex</h1>
+					</header>
 
-          <div className='pokemons'>
-            <button className='pokemon-btn' onClick={this.handlePrevious}>Previous</button>
-            {this.state.pokemon[this.state.index]}
-            <button className='pokemon-btn' onClick={this.handleNext}>Next</button>
-          </div>
-        </div>
-      </section>
-    );
-  }
+					<Filter />
+
+					<div className="pokemons">
+						<button className="pokemon-btn" onClick={this.handlePrevious}>
+							Previous
+						</button>
+						{this.state.pokemon[this.state.index]}
+						<button className="pokemon-btn" onClick={this.handleNext}>
+							Next
+						</button>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Pokedex;
